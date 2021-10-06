1b282f560de515bd0d58851d9dc81a48c51a3178
Emre Ünsal <unsalemre111@gmail.com>
2021-10-06 20:24:19 +0300

    add infinite scroll

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
-import './style.css'
+// key : 2cX7dYaMvSEiuNamTY03eeJupY40BErq
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const apiKey = "2cX7dYaMvSEiuNamTY03eeJupY40BErq";
+
+const bodyDiv = document.getElementById("app");
+const searchTxt = document.getElementById("search");
+
+let requestCount = 0;
+
+async function getGifs(Link) {
+  const response = await fetch(Link);
+  const gifArray = await response.json();
+  console.log(gifArray);
+  if (requestCount > 0) {
+    return renderGifs(gifArray.data, false);
+    requestCount = 0;
+  }
+  renderGifs(gifArray.data, true);
+}
+
+function renderGifs(array, resetBody) {
+  if (resetBody) {
+    bodyDiv.innerHTML = "";
+  }
+  array.forEach((element) => {
+    const image = document.createElement("img");
+    image.src = element.images.downsized.url;
+    bodyDiv.appendChild(image);
+  });
+}
+document.addEventListener("scroll", function (params) {
+  const scrollPosition = window.scrollY;
+  const allWindowHeight = document.body.clientHeight;
+  const windowHeigt = window.innerHeight;
+  const currentBottomLine = scrollPosition + windowHeigt;
+  const requestScrollPosition = allWindowHeight - currentBottomLine;
+  console.log(requestScrollPosition);
+  if (
+    200 > requestScrollPosition &&
+    requestScrollPosition > 100 &&
+    requestCount == 0
+  ) {
+    getGifs(
+      `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&offset=25`
+    );
+    requestCount++;
+    console.log("istek gitti");
+  }
+});
+
+await getGifs(
+  `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`
+);
+
+searchTxt.addEventListener("keyup", function (e) {
+  getGifs(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${e.target.value}&limit=25&offset=0&rating=g`
+  );
+});
