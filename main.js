421ab45cde7d9914b51d1807847bd05720280687
Emre Ünsal <unsalemre111@gmail.com>
2021-10-06 20:25:38 +0300

    add infinite scroll

--- main.js
+++ main.js
@@ -6,16 +6,19 @@ const bodyDiv = document.getElementById("app");
 const searchTxt = document.getElementById("search");
 
 let requestCount = 0;
+let offset = 25;
 
 async function getGifs(Link) {
   const response = await fetch(Link);
   const gifArray = await response.json();
   console.log(gifArray);
   if (requestCount > 0) {
-    return renderGifs(gifArray.data, false);
     requestCount = 0;
+    console.log("sıfırlamadı");
+    return renderGifs(gifArray.data, false);
   }
   renderGifs(gifArray.data, true);
+  console.log("sıfırladı");
 }
 
 function renderGifs(array, resetBody) {
@@ -28,32 +31,40 @@ function renderGifs(array, resetBody) {
     bodyDiv.appendChild(image);
   });
 }
-document.addEventListener("scroll", function (params) {
+document.addEventListener("scroll", function (searchBool) {
   const scrollPosition = window.scrollY;
   const allWindowHeight = document.body.clientHeight;
   const windowHeigt = window.innerHeight;
   const currentBottomLine = scrollPosition + windowHeigt;
   const requestScrollPosition = allWindowHeight - currentBottomLine;
-  console.log(requestScrollPosition);
   if (
     200 > requestScrollPosition &&
     requestScrollPosition > 100 &&
     requestCount == 0
   ) {
-    getGifs(
-      `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&offset=25`
-    );
     requestCount++;
+    offset += 25;
+    getGifs(
+      `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&offset=${offset}`
+    );
+
     console.log("istek gitti");
   }
 });
 
-await getGifs(
-  `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`
-);
+getGifs(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`);
 
 searchTxt.addEventListener("keyup", function (e) {
-  getGifs(
-    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${e.target.value}&limit=25&offset=0&rating=g`
-  );
+  const searchText = e.target.value;
+  console.log(e.target.value),
+    setTimeout(() => {
+      if (searchTxt == "") {
+        return getGifs(
+          `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`
+        );
+      }
+      getGifs(
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTxt}&limit=25&offset=0&rating=g`
+      );
+    }, 3000);
 });
